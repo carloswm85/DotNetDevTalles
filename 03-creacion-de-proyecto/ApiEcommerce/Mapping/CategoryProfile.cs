ace20d107578b6ba8c49f2f29540ec0d6040f813
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2025-10-17 22:21:20 -0300

    add # automapper dependency and CategoryProfile

---
+++ 03-creacion-de-proyecto/ApiEcommerce/Mapping/CategoryProfile.cs
@@ -0,0 +1,14 @@
+
+using ApiCommerce.Models.Dtos;
+using AutoMapper;
+
+namespace ApiCommerce.Mapping;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Category, CreateCategoryDto>().ReverseMap();
+    }
+}
