11cf00aedbb1ce68079a57ce6643d2d65f9a6058
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2025-10-17 21:11:29 -0300

    add  # category dtos

---
+++ 03-creacion-de-proyecto/ApiEcommerce/Models/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,12 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiCommerce.Models.Dtos;
+
+public class CreateCategoryDto
+{
+	[Required(ErrorMessage = "The Name field is required.")]
+	[MaxLength(50, ErrorMessage = "The Name field must not exceed 50 characters.")]
+	[MinLength(3, ErrorMessage = "The Name field must be at least 3 characters.")]
+	public string Name { get; set; } = string.Empty;
+}
--- 03-creacion-de-proyecto/ApiEcommerce/Repository/IRepository/ICategoryRepository.cs
+++ 03-creacion-de-proyecto/ApiEcommerce/Repository/IRepository/ICategoryRepository.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ApiEcommerce.Repository.IRepository;
 
 public interface ICategoryRepository
